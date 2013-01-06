a5c1ae9f664827def0d6a2906cf574765e4ee8dc
LeonFedotov <fedotov.leon@gmail.com>
2013-01-06 11:05:26 +0200

    db connect

--- koans/about_db_connect.rb
+++ koans/about_db_connect.rb
@@ -6,14 +6,14 @@ class AboutDBConnect < EdgeCase::Koan
   def test_connecting_to_a_database
     connection = Mongo::Connection.new
     assert_not_nil connection
-    assert_equal __, connection.class
+    assert_equal Mongo::Connection, connection.class
   end
 
   def test_creating_and_deleting_a_database
     connection = Mongo::Connection.new
     db = connection.db("mongo_koans")
     assert_not_nil db, "DB not available"
-    assert_equal __, db.class
+    assert_equal Mongo::DB, db.class
 
     connection.drop_database("mongo_koans")
   end
